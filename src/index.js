7046c221485f3bd2824579d83931856a2fc0d932
vanhung1999 <vanhung1999dev@gmail.com>
2022-03-12 09:19:24 +0700

    initial webpack config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const object = {
+    field: 'field',
+    property:'property'
+}
+
+
+console.log('object', object);
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+}
